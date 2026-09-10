e706405c8be4e05a8d021533ab1a98775b31a8fb
pintoto <ary.303erfani@gmail.com>
2026-09-10 10:51:00 +0330

    update main.cpp
    add deltaTime

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <cmath>
 #include "pt_audio.hpp"
+
 int main(int argc, char* argv[]) {
     srand(time(0));
     
@@ -21,7 +22,7 @@ int main(int argc, char* argv[]) {
 
 
     );
-
+    Uint32 previous = SDL_GetTicks();
     const int FPS = 30;
     const int frameDelay = 1000 / FPS;
     bool run=true;
@@ -34,6 +35,12 @@ int main(int argc, char* argv[]) {
     SDL_Rect pl_src ={11,9,167,155};
     SDL_Rect pl_dst ={144,104,32,32};
     double pl_angle =0;
+    float playerX = 144.0f;
+    float playerY = 104.0f;
+
+    pl_dst.x = (int)playerX;
+    pl_dst.y = (int)playerY;
+    const float playerSpeed = 90.0f;
 
     SDL_Rect coin_src={203,122,70,70};
     SDL_Rect coin_dst={rand() % 320,rand() % 240,16,16};
@@ -41,7 +48,12 @@ int main(int argc, char* argv[]) {
     SDL_Rect enemy_src={187,3,110,112};
     SDL_Rect enemy_dst={rand() % 320,rand() % 240,22,22};
     float enemy_angle=0;
+    float enemyX = enemy_dst.x;
+    float enemyY = enemy_dst.y;
 
+    enemy_dst.x = (int)enemyX;
+    enemy_dst.y = (int)enemyY;
+    const float enemyspeed=60.0f;
 
     
 
@@ -78,58 +90,69 @@ int main(int argc, char* argv[]) {
         }
         
 
+        //get deltaTime
+        Uint32 current=SDL_GetTicks();
+        float deltaTime = (current - previous) / 1000.0f;
+        previous = current;
+
         SDL_SetRenderDrawColor(ren,0,0,0,255);
         SDL_RenderClear(ren);
-        
+
+
+
         const Uint8* kay=SDL_GetKeyboardState(NULL);
 
-        
-    
+
     
         if(kay[SDL_SCANCODE_W]) {
 
             pl_angle =360;
-            pl_dst.y -= 3;
+            playerY -= playerSpeed * deltaTime;
 
         }else if(kay[SDL_SCANCODE_S]) {
 
             pl_angle =180;
-            pl_dst.y += 3;
+            playerY += playerSpeed * deltaTime;
 
 
         }else if(kay[SDL_SCANCODE_D]) {
 
             pl_angle=90;
-            pl_dst.x += 3;
+            playerX += playerSpeed * deltaTime;
 
         }else if(kay[SDL_SCANCODE_A]) {
 
             pl_angle= -90;
-            pl_dst.x -= 3;
+            playerX -= playerSpeed * deltaTime;
 
 
         }
 
-        if(pl_dst.x < 10) {
+        if(playerX < 10) {
 
-            pl_dst.x=10;
+            playerX=10;
+            
 
-        }else if(pl_dst.x > 280) {
+        }else if(playerX > 280) {
 
-            pl_dst.x = 280;
-
-        }
-
-        if(pl_dst.y < 2) {
-
-            pl_dst.y=2;
-
-        }else if(pl_dst.y > 208) {
-
-            pl_dst.y=208;
+            playerX = 280;
 
         }
 
+        if(playerY < 2) {
+
+            playerY = 2;
+
+        }else if(playerY > 208) {
+
+            playerY=208;
+
+        }
+
+
+        pl_dst.x = (int)playerX;
+        pl_dst.y = (int)playerY;
+    
 
         if(checkCollision(pl_dst,coin_dst)) {
             pt_audio::play_tone_ms_async(1800,500);
@@ -138,7 +161,6 @@ int main(int argc, char* argv[]) {
 
         }
 
-
         if(checkCollision(enemy_dst,pl_dst)) {
 
             pt_audio::play_tone_ms_async(3500,500);
@@ -146,18 +168,23 @@ int main(int argc, char* argv[]) {
 
         }
 
-    float dx = pl_dst.x - enemy_dst.x;
-    float dy = pl_dst.y - enemy_dst.y;
+    float dx = playerX - enemyX;
+    float dy = playerY - enemyY;
 
     float distance = sqrt(dx * dx + dy * dy);
 
     if (distance > 0) {
      
 
-        enemy_dst.x += dx / distance * 2.0f;
-        enemy_dst.y += dy / distance * 2.0f;
+        enemyX += dx / distance * enemyspeed * deltaTime;
+        enemyY += dy / distance * enemyspeed * deltaTime;
     }
+    
+    
 
+    enemy_dst.x = (int)enemyX;
+    enemy_dst.y = (int)enemyY;
+        
 
 
         SDL_RenderCopy(ren,bak_tex,NULL,&bak);
@@ -182,7 +209,7 @@ int main(int argc, char* argv[]) {
         SDL_Delay(frameDelay - frameTime);
 
 
-    }//end FPS
+    }
 
 
 
